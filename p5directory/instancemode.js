fe992131c938b119ffff4d161c2be85e1e44f004
tom4028 <alba4028@o2.pl>
2016-10-27 20:43:26 +0200

    Add files via upload

---
+++ p5directory/instancemode.js
@@ -0,0 +1,48 @@
+
+
+var sketch1 = function(p){
+  p.x = 100;
+  p.y = 100;
+
+  p.setup = function(){
+      p.createCanvas(200,200);
+      p.background(51);
+  }
+  p.draw = function(){
+    p.fill(255,0,200,25);
+    p.noStroke();
+    p.ellipse(p.x,p.y,48,48);
+
+    p.x = p.x +p.random(-10,10);
+    p.y = p.y+p.random(-10,10);
+  }
+}
+
+var sketch2 = function(p){
+  p.x = 100;
+  p.y = 100;
+
+  p.setup = function(){
+      p.createCanvas(200,200);
+      p.background(51);
+  }
+  p.draw = function(){
+    p.fill(255,200,0,25);
+    p.noStroke();
+    p.ellipse(p.x,p.y,48,48);
+
+    p.x = p.x +p.random(-10,10);
+    p.y = p.y+p.random(-10,10);
+  }
+}
+
+
+function resetBackground(){
+  myp5_1.x = myp51.width/2;
+  myp5_1.y = myp51.height/2;
+  myp5_1.background(51);
+}
+var myp5_1 = new p5(sketch1);
+var myp5_2 = new p5(sketch2);
+
+setInterval(resetBackground,3000);
